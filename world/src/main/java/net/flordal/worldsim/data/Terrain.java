f9e10b2227224a20604dd55adc68f7e20c073b4e
HenrikF <henrik.flordal@tacton.com>
2016-07-08 22:23:36 +0200

    Start

---
+++ world/src/main/java/net/flordal/worldsim/data/Terrain.java
@@ -0,0 +1,17 @@
+package net.flordal.worldsim.data;
+
+/**
+ * Created by HenrikF on 2016-07-08.
+ */
+public abstract class Terrain {
+
+    private int altitude;
+
+
+    public abstract TerrainType getTerrainType();
+
+
+
+
+
+}
---
+++ world/src/main/java/net/flordal/worldsim/data/TerrainType.java
@@ -0,0 +1,9 @@
+package net.flordal.worldsim.data;
+
+/**
+ * Created by HenrikF on 2016-07-08.
+ */
+public enum TerrainType {
+
+    
+}
